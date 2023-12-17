7782f59cc884fea4433fe9dd5ccb029f4116b617
kumar <85148531+kumar367316@users.noreply.github.com>
2023-12-17 16:35:00 +0530

    Update EmailService.java

--- src/main/java/com/java/email/api/service/EmailService.java
+++ src/main/java/com/java/email/api/service/EmailService.java
@@ -38,7 +38,6 @@ public class EmailService {
 		MailResponse response = new MailResponse();
 		MimeMessage message = sender.createMimeMessage();
 		try {
-			// set mediaType
 			MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
 					StandardCharsets.UTF_8.name());
 			// add attachment
